7afc634aabcc51dd36b762bf4cc9f09400e7aef2
Tim Guo <guoqikun@163.com>
2014-03-14 20:19:03 +0800

    make Qiniu works

--- app/controllers/mo_site_controller.rb
+++ app/controllers/mo_site_controller.rb
@@ -17,7 +17,7 @@ class MoSiteController < ApplicationController
   end
 
   def item
-    @mo_item.click_times = @mo_item.click_times + 1
+    @mo_item.hit
     #if @mo_item.type == "image"
   end
 
--- app/models/mo_item.rb
+++ app/models/mo_item.rb
@@ -11,9 +11,19 @@ class MoItem < ActiveRecord::Base
   counter :click_times
   counter :click_times_week
 
-  def file_url
-    token = Qiniu::RS.generate_download_token :expires_in => 1800, :pattern => "cmw-rails.qiniudn.com#{file}"
-    'http://cmw-rails.qiniudn.com/uploads/' + file + '?token=' + token
+  def file_url(time, size)
+    # New method accoding to API doc
+    # @access_key = Qiniu::Conf.settings[:access_key]
+    # @secret_key = Qiniu::Conf.settings[:secret_key]
+
+    # @mac = Qiniu::Auth::Digest::Mac.new(@access_key, @secret_key)
+    # base_url = Qiniu::Rs.make_base_url("a.qiniudn.com", "down.jpg")
+    # get_policy = Qiniu::Rs::GetPolicy.new
+    # get_policy.Expires = time
+    # url = get_policy.make_request(base_url, @mac)
+
+    token = Qiniu::RS.generate_download_token :expires_in => time, :pattern => "cmw-rails.qiniudn.com/uploads/#{File.basename(file.path)}-#{size}"
+    file.to_s + '-' + size + '?token=' + token
   end
 
   def isImage?
@@ -24,24 +34,23 @@ class MoItem < ActiveRecord::Base
   end
 
   def hit
-    click_times.increment;
-    click_times_week.increment;
+    click_times.increment
+    click_times_week.increment
   end
 
-  def daily_refresh
-    temp = Random.rand(1...MoItem.count)
-    $redis.set('daily_item', temp)
+  def MoItem.daily_refresh
+    $redis.set('daily_item_id', MoItem.all.sample.id)
   end
 
-  def weekly_refresh
+  def MoItem.weekly_refresh
     MoItem.All.each {|i| i.click_times_week.reset }
   end
 
-  def top_ten
+  def MoItem.top_ten
     MoItem.find(:all, :order => "click_times desc", :limit => 10).reverse
   end
 
-  def top_ten_week
+  def MoItem.top_ten_week
     MoItem.find(:all, :order => "click_times_week desc", :limit => 10).reverse
   end
 end
